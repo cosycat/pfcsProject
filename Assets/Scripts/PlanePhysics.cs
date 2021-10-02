564b09382c1778f7a7ec787d122bcebcece62a29
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2021-10-02 15:17:02 +0200

    Start working on input control for plane turning

--- Assets/Scripts/PlanePhysics.cs
+++ Assets/Scripts/PlanePhysics.cs
@@ -5,4 +5,26 @@ public class PlanePhysics : MonoBehaviour
 {
     public Vector3 Normal { get; private set; } = new Vector3(-1, 1, 0).normalized;
     public float d { get; private set; } = 0;
+    
+    #region Unity Variables
+
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
+
+
+    #endregion
+
+    private void Awake()
+    {
+        _meshFilter = gameObject.GetComponent<MeshFilter>();
+        _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        var normal = _meshFilter.transform.TransformDirection(_meshFilter.mesh.normals[0]);
+        
+    }
+    
+    
 }
