153f75a5f4ec843e248afb4f4e9facd6fc1b1f4a
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2021-10-02 13:51:01 +0200

    Turn plane 45 degree

--- Assets/Scripts/PlanePhysics.cs
+++ Assets/Scripts/PlanePhysics.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 
 public class PlanePhysics : MonoBehaviour
 {
-    public Vector3 Normal { get; private set; } = Vector3.up;
+    public Vector3 Normal { get; private set; } = new Vector3(-1, 1, 0).normalized;
     public float d { get; private set; } = 0;
 }
