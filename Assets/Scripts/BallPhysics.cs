cde9b0f0618f327088b74eda6df50d3fb18977b9
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2021-10-02 11:50:22 +0200

    Move scripts to global namespace

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -1,55 +1,52 @@
 using UnityEngine;
 
-namespace Exercise_2_8
+public class BallPhysics : MonoBehaviour
 {
-    public class BallPhysics : MonoBehaviour
+
+    private Vector3 _velocity = Vector3.zero;
+    private static readonly Vector3 g = new Vector3(0, -9.81f, 0);
+    public float Radius => _sphereCollider.radius;
+        
+        
+
+    #region Unity Variables
+
+    private SphereCollider _sphereCollider;
+    private PlanePhysics plane;
+
+    #endregion
+
+    private void Awake()
     {
-
-        private Vector3 _velocity = Vector3.zero;
-        private static readonly Vector3 g = new Vector3(0, -9.81f, 0);
-        public float Radius => _sphereCollider.radius;
-        
-        
-
-        #region Unity Variables
-
-        private SphereCollider _sphereCollider;
-        private PlanePhysics plane;
-
-        #endregion
-
-        private void Awake()
-        {
-            _sphereCollider = GetComponent<SphereCollider>();
-            plane = FindObjectOfType<PlanePhysics>();
-        }
-
-
-        private void FixedUpdate()
-        {
-            var t = Time.fixedDeltaTime;
-            var newPosition = transform.position + _velocity * t + 0.5f * g * t * t;
-            if (CheckForCollision(newPosition))
-            {
-                _velocity = -_velocity;
-                // todo new newPosition
-            }
-            else
-            {
-                transform.position = newPosition;
-            }
-            ApplyGravitation(t);
-        }
-
-        private bool CheckForCollision(Vector3 newPosition)
-        {
-            return newPosition.y - Radius < 0;
-        }
-
-        private void ApplyGravitation(float t)
-        {
-            _velocity += g * t;
-        }
-    
+        _sphereCollider = GetComponent<SphereCollider>();
+        plane = FindObjectOfType<PlanePhysics>();
     }
+
+
+    private void FixedUpdate()
+    {
+        var t = Time.fixedDeltaTime;
+        var newPosition = transform.position + _velocity * t + 0.5f * g * t * t;
+        if (CheckForCollision(newPosition))
+        {
+            _velocity = -_velocity;
+            // todo new newPosition
+        }
+        else
+        {
+            transform.position = newPosition;
+        }
+        ApplyGravitation(t);
+    }
+
+    private bool CheckForCollision(Vector3 newPosition)
+    {
+        return newPosition.y - Radius < 0;
+    }
+
+    private void ApplyGravitation(float t)
+    {
+        _velocity += g * t;
+    }
+    
 }
--- Assets/Scripts/PlanePhysics.cs
+++ Assets/Scripts/PlanePhysics.cs
@@ -1,11 +1,8 @@
 using System;
 using UnityEngine;
 
-namespace Exercise_2_8
+public class PlanePhysics : MonoBehaviour
 {
-    public class PlanePhysics : MonoBehaviour
-    {
-        public Vector3 Normal { get; private set; } = Vector3.up;
-        public float d { get; private set; } = 0;
-    }
+    public Vector3 Normal { get; private set; } = Vector3.up;
+    public float d { get; private set; } = 0;
 }
