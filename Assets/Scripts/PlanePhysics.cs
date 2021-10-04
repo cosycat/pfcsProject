14db1c423aaf00832a01de0efa58f1fad5ee60dd
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2021-10-04 13:46:06 +0200

    add to previous commit

--- Assets/Scripts/PlanePhysics.cs
+++ Assets/Scripts/PlanePhysics.cs
@@ -1,16 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlanePhysics : MonoBehaviour
 {
-    public Vector3 Normal { get; private set; } = new Vector3(-1, 1, 0).normalized;
+    public Vector3 Normal => _meshFilter.transform.TransformDirection(_meshFilter.mesh.normals[0]);
     public float d { get; private set; } = 0;
-    
+
+    private Vector2 _turnMovement;
+    [SerializeField] private float turnSpeed = 1f;
+
     #region Unity Variables
 
     private MeshFilter _meshFilter;
     private MeshRenderer _meshRenderer;
-
 
     #endregion
 
@@ -20,11 +23,26 @@ public class PlanePhysics : MonoBehaviour
         _meshRenderer = gameObject.GetComponent<MeshRenderer>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        var normal = _meshFilter.transform.TransformDirection(_meshFilter.mesh.normals[0]);
-        
+        transform.Rotate(Vector3.forward, _turnMovement.x * turnSpeed);
     }
-    
-    
+
+    public void OnTurn(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                _turnMovement = context.ReadValue<Vector2>();
+                break;
+            case InputActionPhase.Canceled:
+                _turnMovement = context.ReadValue<Vector2>();
+                break;
+        }
+    }
+
+    public void OnReset(InputAction.CallbackContext context)
+    {
+        transform.rotation = Quaternion.identity;
+    }
 }
