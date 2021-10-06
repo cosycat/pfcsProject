6e3e26ab9988dd150a6a5510dafc0dda59a4eb02
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2021-10-06 16:29:38 +0200

    Move CameraController to Namespace Series2

---
+++ Assets/Scripts/Series2/CameraController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Series2
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Camera _camera;
+        public Vector3 StartPosition { get; private set; }
+        [SerializeField] private float zoomSpeed = 1f;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            StartPosition = transform.position;
+        }
+
+        public void OnZoom(InputAction.CallbackContext context)
+        {
+            var newPos = transform.position;
+            newPos.z += zoomSpeed * context.ReadValue<float>();
+            transform.position = newPos;
+        }
+
+        public void OnReset(InputAction.CallbackContext context)
+        {
+            transform.position = StartPosition;
+        }
+    
+    }
+}
