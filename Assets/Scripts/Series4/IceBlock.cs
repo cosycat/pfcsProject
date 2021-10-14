ebc60b5ab2464ca163ab86e3ff3873beea1b044c
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2021-10-14 20:09:20 +0200

    Copy plane physics from Series2
    Add Ice Block and Material

---
+++ Assets/Scripts/Series4/IceBlock.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Series4
+{
+    public class IceBlock : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
